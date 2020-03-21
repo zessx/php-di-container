54431426dd7bb207766838e166f65760fd94978a
zessx <contact@zes.sx>
2020-03-21 19:43:40 +0100

    Initial commit

---
+++ src/Container.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ZCode\DI;
+
+class Container
+{
+
+}
---
+++ tests/BaseContainerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ZCode\DI\Test;
+
+use PHPUnit\Framework\TestCase;
+
+class BaseContainerTest extends TestCase
+{
+    public function __construct()
+    {
+
+    }
+}
